ffc4cbde03520bc03a5ed6985fce067dae753577
nyghtowl <warrick.melanie@gmail.com>
2013-07-07 00:59:46 -0700

    Setup sessions and signin failure.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ SampleApp::Application.routes.draw do
 
   # match '/', to: 'static_pages#home'- is one way but one below better
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help', to: 'static_pages#help'
 
