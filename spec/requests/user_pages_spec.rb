73383243153deecce54d0c9e3d15d2845c6bb858
nyghtowl <warrick.melanie@gmail.com>
2013-07-12 01:49:48 -0700

    Alignment to github files at mhartl.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,52 @@ describe "User pages" do
 
   subject { page }
 
+
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all)  { User.delete_all }
+      
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1',    text: 'All users') }
+
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end  
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+
   describe "Sign up page" do
       before { visit signup_path }
 
@@ -36,6 +82,7 @@ describe "User pages" do
 
           it { should have_selector('title', text: 'Sign up') }
           it { should have_content('error') }
+          it { should_not have_content('Password digest') }
         end
       end
 
@@ -58,12 +105,7 @@ describe "User pages" do
 
           it { should have_selector('title', text: user.name) }
           it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-          it { should have_link('Sign out') }
-          
-          describe "followed by signout" do
-          before { click_link "Sign out" }
-            it { should have_link('Sign in') }
-          end
+          it { should have_link('Sign out') }          
         end
       end
   end
@@ -90,6 +132,7 @@ describe "User pages" do
     describe "with valid information" do
       let(:new_name)  { "New Name" }
       let(:new_email) { "new@example.com" }
+
       before do
         fill_in "Name",             with: new_name
         fill_in "Email",            with: new_email
@@ -106,51 +149,5 @@ describe "User pages" do
     end
   end
 
-  describe "index" do
-    let(:user) { FactoryGirl.create(:user) }
-
-    before(:each) do
-      sign_in user
-      visit users_path
-    end
-
-    it { should have_selector('title', text: 'All users') }
-    it { should have_selector('h1',    text: 'All users') }
-
-    describe "pagination" do
-
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all)  { User.delete_all }
-
-      it { should have_selector('div.pagination') }
-
-      it "should list each user" do
-        User.all.each do |user|
-          page.should have_selector('li', text: user.name)
-        end
-      end
-    end  
-
-
-
-    describe "delete links" do
-
-      it { should_not have_link('delete') }
-
-      describe "as an admin user" do
-        let(:admin) { FactoryGirl.create(:admin) }
-        before do
-          sign_in admin
-          visit users_path
-        end
-
-        it { should have_link('delete', href: user_path(User.first)) }
-        it "should be able to delete another user" do
-          expect { click_link('delete') }.to change(User, :count).by(-1)
-        end
-        it { should_not have_link('delete', href: user_path(admin)) }
-      end
-    end
-  end
 
 end
